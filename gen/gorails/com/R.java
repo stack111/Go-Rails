f7f8b371f7f120657dbae6b3a0b18a809cb338d2
dan <dan.stack111@gmail.com>
2011-05-11 21:54:29 -0500

    tesr

--- gen/gorails/com/R.java
+++ gen/gorails/com/R.java
@@ -19,12 +19,16 @@ public final class R {
         public static final int HScroll=0x7f050000;
         public static final int ModeChangeButton=0x7f050004;
         public static final int TopBarLayout=0x7f050003;
+        public static final int ViewTrains=0x7f05000f;
         public static final int button1=0x7f05000a;
-        public static final int destSpinner=0x7f05000b;
+        public static final int destSpinner=0x7f05000d;
         public static final int imageView1=0x7f050002;
         public static final int linearLayout2=0x7f050001;
+        public static final int stationLabel=0x7f05000b;
         public static final int tableLayout1=0x7f050007;
         public static final int tableRow1=0x7f050008;
+        public static final int tableRow2=0x7f05000c;
+        public static final int tableRow3=0x7f05000e;
         public static final int textView1=0x7f050009;
     }
     public static final class layout {
